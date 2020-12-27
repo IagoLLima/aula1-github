f0f5bf29fb79513f24720ae29fe0d32f8a7805fa
Iago Lima <iago.lopes1@hotmail.com>
2020-12-27 10:22:18 -0300

    Atualização do 1° programa

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,23 @@
 package application;
 
+import java.util.Date;
+
+import entities.Order;
+import entities.enums.OrderStatus;
+
 public class Program {
   public static void main(String[] args) {
-    System.out.println("Bom dia");
+
+    Order order = new Order(1080,new Date(),OrderStatus.PROCESSING);
+
+    //convertando uma Sting informado pelo usário em enum
+    OrderStatus os1 = OrderStatus.DELIVERED;
+
+    OrderStatus os2 = OrderStatus.valueOf("DELIVERED");//conversão é esse comando
+    System.out.println(os1);
+    System.out.println(os2);
+
+    System.out.println(order);
+
   }
 }
